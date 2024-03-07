37350f17eaa112d29e54bbf3093cd7f542910144
webapis <60083568+webapis@users.noreply.github.com>
2024-03-07 12:08:43 +0500

    init

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 // For more information, see https://crawlee.dev/
+import dotenv from 'dotenv';
 import { PuppeteerCrawler, ProxyConfiguration } from 'crawlee';
 import { router } from './routes.js';
-
-const startUrls = ['https://crawlee.dev'];
+import preNavigationHooks from './crawler-helper/preNavigationHooks.js';
+dotenv.config({ silent: true });
+debugger
+const brand = process.env.brand
+const {  url:startUrls } = await import(`./handlers/${brand}.js`)
+debugger
 
 const crawler = new PuppeteerCrawler({
     // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
     requestHandler: router,
     // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
+    maxRequestsPerCrawl: 0,
+    preNavigationHooks
 });
 
 await crawler.run(startUrls);
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,54 @@
+import dotenv from 'dotenv';
 import { createPuppeteerRouter } from 'crawlee';
+dotenv.config({ silent: true });
+
+const brand = process.env.brand
+const { default: handler, pSelector, dpSelector, phref, dphref } = await import(`./handlers/${brand}.js`)
+debugger
+
 
 export const router = createPuppeteerRouter();
 
 router.addDefaultHandler(async ({ enqueueLinks, log }) => {
     log.info(`enqueueing new URLs`);
-    await enqueueLinks({
-        globs: ['https://crawlee.dev/**'],
-        label: 'detail',
+ const result =   await enqueueLinks({
+        selector: phref,
+        label: 'list',
     });
+
+
 });
 
-router.addHandler('detail', async ({ request, page, log, pushData }) => {
+
+router.addHandler('list', async ({ request, page, log, pushData,enqueueLinks }) => {
     const title = await page.title();
     log.info(`${title}`, { url: request.loadedUrl });
 
+debugger
+    const result =   await enqueueLinks({
+        selector: dphref,
+        label: 'detail',
+    });
+    debugger
     await pushData({
         url: request.loadedUrl,
         title,
     });
+    debugger
+console.log('LIST HANDLED',title)
+    
+});
+router.addHandler('detail', async ({ request, page, log, pushData }) => {
+    const title = await page.title();
+    log.info(`${title}`, { url: request.loadedUrl });
+
+
+   // handler()
+    debugger
+    await pushData({
+        url: request.loadedUrl,
+        title,
+    });
+
+    console.log('DETAIL HANDLED',title)
 });
