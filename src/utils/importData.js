3a3a048cae8d51bdc00c8883ce2020807ff14bff
webapis <60083568+webapis@users.noreply.github.com>
2024-03-11 10:26:53 +0500

    alg

---
+++ src/utils/importData.js
@@ -0,0 +1,67 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+require("dotenv").config();
+const algoliasearch = require("algoliasearch");
+
+const client = algoliasearch("7JF244QSZZ", process.env.ALGOLIAKEY);
+const marka = process.env.marka
+
+// function setSettings(index) {
+//   return new Promise((resolve, reject) => {
+//     try {
+//       index
+//         .setSettings({
+//           attributeForDistinct: "brand",
+//           distinct: true,
+//         })
+//         .then(() => {
+//           return resolve(true);
+//         });
+//     } catch (error) {
+//       return reject(error);
+//     }
+//   });
+// }
+
+async function importLinkData({ data,brand }) {
+  const linkIndex = client.initIndex("product");
+  
+
+  let hits = [];
+ const result = await linkIndex.browseObjects({
+  batch: batch => {
+    hits = hits.concat(batch);
+  },
+  attributesToRetrieve: ["objectID"],
+  query: brand
+})
+debugger
+const objectsToDelete = hits.map(m=>m.objectID)
+debugger
+await linkIndex.deleteObjects(objectsToDelete)
+debugger
+//  await setSettings(linkIndex);
+  return new Promise((resolve, reject) => {
+    try {
+      linkIndex
+        .saveObjects(data, { autoGenerateObjectIDIfNotExist: true })
+        .then((d) => {
+          return resolve(d);
+        });
+    } catch (error) {
+      return reject(error);
+    }
+  });
+}
+
+
+
+if(data.length>0){
+  await importLinkData({data:data.map(m=>{return {...m,brand:marka}}),brand:marka })
+}else{
+  throw 'No Items found to upload'
+}
+
+
+
