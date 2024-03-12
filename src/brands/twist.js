031d989e3907845f020fbe24dccb77d77ad30d65
webapis <60083568+webapis@users.noreply.github.com>
2024-03-12 16:19:39 +0500

    twist

---
+++ src/brands/twist.js
@@ -0,0 +1,33 @@
+
+const pSelector = '.prd-list'
+
+const phref = '.menu a'
+
+const url = ['https://www.twist.com.tr/']
+
+
+export { pSelector, phref, url }
+
+
+export default async function twist({ page }) {
+    debugger
+    const data = await page.$$eval('.prd-inner', (documents) => {
+
+        return documents.map(document => {
+            return {
+                image: Array.from(document.querySelectorAll('[data-large]')).map(m=>m.getAttribute('data-large')),
+                title: document.querySelector('.prd-name').innerText,
+                price: document.querySelector('.urunListe_satisFiyat').innerText.replace('₺',''),
+                link: document.querySelector('.prd-lnk').href
+            }
+        })
+
+    })
+
+    debugger
+    return data
+
+}
+
+
+
