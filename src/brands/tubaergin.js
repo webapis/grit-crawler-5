ccefc6945c11b44036e728ab03667118f1d56022
webapis <60083568+webapis@users.noreply.github.com>
2024-03-12 16:54:57 +0500

    tubaergin

---
+++ src/brands/tubaergin.js
@@ -0,0 +1,33 @@
+
+const pSelector = '#product-list-container'
+
+const phref = '#navigation a'
+
+const url = ['https://www.tubaergin.com/']
+
+
+export { pSelector, phref, url }
+
+
+export default async function adl({ page }) {
+    debugger
+    const data = await page.$$eval('.showcase', (documents) => {
+
+        return documents.map(document => {
+            return {
+                image: 'https:'+ document.querySelector('.showcase-image img').getAttribute('data-src'),
+                title: document.querySelector('.showcase-title a').innerText,
+                price: document.querySelector('.showcase-price-new').innerText.replace('₺',''),
+                link: document.querySelector('.showcase-label-container').href
+            }
+        })
+
+    })
+
+    debugger
+    return data
+
+}
+
+
+
