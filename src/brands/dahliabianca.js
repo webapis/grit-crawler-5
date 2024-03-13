173f6fc6bc1e1b7328aaec2d6b80ca58067c754b
webapis <60083568+webapis@users.noreply.github.com>
2024-03-13 08:44:08 +0500

    dahliabianca

---
+++ src/brands/dahliabianca.js
@@ -0,0 +1,35 @@
+import autoscroller from '../utils/autoscroll.js'
+const pSelector = '#ajax-product-grid'
+
+const phref = '.top-menu a'
+
+const url = ['https://www.dahliabianca.com/']
+
+
+export { pSelector, phref, url }
+
+
+export default async function dahliabianca({ page }) {
+    debugger
+    await autoscroller(page,150)
+    const data = await page.$$eval('.product-item', (documents) => {
+
+        return documents.map(document => {
+            return {
+                image: document.querySelector('.product-item img').getAttribute('data-src'),
+                title: document.querySelector('.product-title').innerText,
+                price: document.querySelector('.actual-price').innerText.replace('₺',''),
+                link: document.querySelector('.product-title a').href,
+                currency:'TL'
+            }
+        })
+
+    })
+
+    debugger
+    return data
+
+}
+
+
+
