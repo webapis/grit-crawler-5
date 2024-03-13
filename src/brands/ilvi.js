f07bb293f6e73db526661e25003e4c2439f17d7d
webapis <60083568+webapis@users.noreply.github.com>
2024-03-13 11:19:15 +0500

    Create ilvi.js

---
+++ src/brands/ilvi.js
@@ -0,0 +1,34 @@
+
+const pSelector = '#divIcerik'
+
+const phref = '.AcilirMenu a'
+
+const url = ['https://www.ilvi.com/']
+
+
+export { pSelector, phref, url }
+
+
+export default async function adl({ page }) {
+    debugger
+    const data = await page.$$eval('.ItemOrj', (documents) => {
+
+        return documents.map(document => {
+            return {
+                image: [document.querySelector('[data-original]').getAttribute('data-original')],
+                title: document.querySelector('.productName.detailUrl a').innerText,
+                price: document.querySelector('.discountPrice span').innerText.replace('₺',''),
+                link: document.querySelector('.productName.detailUrl a').href,
+                currency:'TL'
+            }
+        })
+
+    })
+
+    debugger
+    return data
+
+}
+
+
+
