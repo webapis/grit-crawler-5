30b9e2a4ecd8ec8e079cd276abec6a282bd29d45
Serdar Ashirov <webapis@users.noreply.github.com>
2024-04-23 17:10:51 +0500

    birelin

---
+++ src/brands/birelin.js
@@ -0,0 +1,39 @@
+import autoScroll from "../utils/autoscroll.js"
+const pSelector = '.ProductListContent '
+
+const phref = '.navigation a'
+
+const url = ['https://www.birelin.com/']
+
+
+export { pSelector, phref, url }
+
+
+export default async function fever({ page }) {
+    await autoScroll(page, 50)
+    debugger
+    const data = await page.$$eval('.productItem', (documents) => {
+
+        return documents.map(document => {
+
+            try {
+                return {
+                    image: [document.querySelector('[data-original]').getAttribute('data-original')],
+                    title: document.querySelector('.productName.detailUrl').innerText,
+                    price: document.querySelector('.discountPrice').innerText.replace('₺', ''),
+                    link: document.querySelector('.productName.detailUrl a').href,
+                    currency: 'TL'
+                }
+            } catch (error) {
+                return { error: error.toString(), content: document.innerHTML }
+            }
+
+        })
+
+    })
+
+    debugger
+    return data
+
+}
+
