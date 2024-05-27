b88fe7beb3da0ae4d73f1ba035d3a1113f5bd9ca
Serdar Ashirov <webapis@users.noreply.github.com>
2024-05-27 15:15:34 +0500

    Update kasha.js

--- src/brands/kasha.js
+++ src/brands/kasha.js
@@ -13,14 +13,15 @@ export default async function kasha({ page }) {
     await autoScroll(page, 50)
     debugger
     const data = await page.$$eval('[data-id]', (documents) => {
-
+            const selPrice =document.querySelector('.sell-price') ?document.querySelector('.sell-price').innerText.replace('₺','').trim():null
+            const discount =document.querySelector('.discount-price')?Array.from(document.querySelector('.discount-price').querySelectorAll('span')).reverse()[0].innerText.replace('₺','').trim():null
         return documents.map(document => {
 
             try {
                 return {
                     image: [document.querySelector('[srcset]').getAttribute('srcset').split(',')[10].split(' ').filter((f)=>f)[0]],
                     title: document.querySelector('.product-name').innerText,
-                    price: Array.from(document.querySelector('.discount-price').querySelectorAll('span')).reverse()[0].innerText.replace('₺','').trim(),
+                    price: selPrice? selPrice:discount,
                     link: document.querySelector('a').href,
                     currency: 'TL'
                 }
