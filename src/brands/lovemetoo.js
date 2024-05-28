2fe41c56f42f473b7b51ac164a986e3a2a5e5912
Serdar Ashirov <webapis@users.noreply.github.com>
2024-05-28 09:33:33 +0500

    lovemetoo

---
+++ src/brands/lovemetoo.js
@@ -0,0 +1,47 @@
+import autoscroll from '../utils/autoscroll.js'
+const pSelector = '.product-list'
+
+const phref = '.navigation a'
+
+const url = ['https://lovemetoo.com.tr/']
+
+
+export { pSelector, phref, url }
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+export default async function lovemetoo({ page, enqueueLinks, request, log }) {
+    debugger
+    const title = await page.title();
+    log.info(`COLLECT ${title}`, { url: request.loadedUrl });
+    await page.waitForSelector(pSelector)
+    debugger
+
+ 
+    await autoscroll(page, 50)
+    const data = await page.$$eval('.product-block', (documents) => {
+
+        return documents.map(document => {
+            try {
+                return {
+                    image: ['https:'+document.querySelector('[data-srcset]').getAttribute('data-srcset').split(',')[5].split(' ').filter(f=>f)[0]],
+                    title: document.querySelector('.product-block__title').innerText,
+                    price: document.querySelector('.money').innerText.replace('TL','').trim(),
+                    link: document.querySelector('.product-link').href,
+                    currency: 'TL'
+                }
+            } catch (error) {
+                return { error: error.toString() }
+            }
+
+        })
+    })
+
+    debugger
+    return data
+
+}
+
+
+
