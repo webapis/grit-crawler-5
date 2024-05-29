4b0d3370aa5e74f9200642c51323e88c13431082
Serdar Ashirov <webapis@users.noreply.github.com>
2024-05-29 14:30:35 +0500

    Update silkandcashmere.js

--- src/brands/silkandcashmere.js
+++ src/brands/silkandcashmere.js
@@ -18,7 +18,7 @@ export default async function silkandcashmere({ page }) {
        
             try {
                 return {
-                    image: ['https:'+ document.querySelector(".image__img[srcset]").getAttribute("srcset").split(",")[10]],
+                    image: ['https:'+ document.querySelector(".image__img[srcset]").getAttribute("srcset").split(",")[10].split(' ').filter(f=>f)[0]],
                     title: document.querySelector('.product-item__product-title a').innerText,
                     price: document.querySelector('.product-item__price .money').innerText.replace('₺',''),
                     link: document.querySelector('.product-item__product-title a').href,
