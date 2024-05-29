9c29349b05eae07e1e388495aaed8dde0417577d
Serdar Ashirov <webapis@users.noreply.github.com>
2024-05-29 14:01:50 +0500

    love-onfriday

---
+++ src/brands/silkandcashmere.js
@@ -0,0 +1,39 @@
+import autoScroll from "../utils/autoscroll.js"
+const pSelector = '.collection__window'
+
+const phref = '.header__links.header__links-primary a'
+
+const url = ['https://silkandcashmere.com/']
+
+
+export { pSelector, phref, url }
+
+
+export default async function silkandcashmere({ page }) {
+    await autoScroll(page, 50)
+    debugger
+    const data = await page.$$eval('.product-item', (documents) => {
+
+        return documents.map(document => {
+       
+            try {
+                return {
+                    image: ['https:'+ document.querySelector(".image__img[srcset]").getAttribute("srcset").split(",")[10]],
+                    title: document.querySelector('.product-item__product-title a').innerText,
+                    price: document.querySelector('.product-item__price .money').innerText.replace('₺',''),
+                    link: document.querySelector('.product-item__product-title a').href,
+                    currency: 'TL'
+                }
+            } catch (error) {
+                return { error: error.toString(), content: document.innerHTML }
+            }
+
+        })
+
+    })
+
+    debugger
+    return data
+
+}
+
